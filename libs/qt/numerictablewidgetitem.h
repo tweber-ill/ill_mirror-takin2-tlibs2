c195bbb1236d8081194e6bc56d4213e797464a8e
Tobias WEBER <tweber@ill.fr>
2021-06-25 19:01:09 +0200

    cosmetic changes

--- libs/qt/numerictablewidgetitem.h
+++ libs/qt/numerictablewidgetitem.h
@@ -18,17 +18,28 @@ template<class T = double>
 class NumericTableWidgetItem : public QTableWidgetItem
 {
 public:
+	/**
+	 * construct a numeric table widget item from a numeric value
+	 */
 	NumericTableWidgetItem(const T& val, std::streamsize prec = 6)
 		: QTableWidgetItem(tl2::var_to_str(val, prec).c_str()),
 		  m_val{val}, m_prec{prec}
 	{}
 
+
+	/**
+	 * construct a numeric table widget item from a string value
+	 */
 	NumericTableWidgetItem(const QString& val, std::streamsize prec = 6)
 		: QTableWidgetItem(val),
 		  m_val{tl2::str_to_var<T>(val.toStdString())},
 		  m_prec{prec}
 	{}
 
+
+	/**
+	 * compare items
+	 */
 	virtual bool operator<(const QTableWidgetItem& item) const override
 	{
 		T val1 = tl2::str_to_var<T>(text().toStdString());
@@ -37,6 +48,10 @@ public:
 		return val1 < val2;
 	}
 
+
+	/**
+	 * get a cloned copy of this table item widget
+	 */
 	virtual QTableWidgetItem* clone() const override
 	{
 		auto item = new NumericTableWidgetItem<T>(m_val);
@@ -44,6 +59,10 @@ public:
 		return item;
 	};
 
+
+	/**
+	 * set user data
+	 */
 	virtual void setData(int itemdatarole, const QVariant& var) override
 	{
 		m_val = var.value<T>();
@@ -51,10 +70,21 @@ public:
 		QTableWidgetItem::setData(itemdatarole, str.c_str());
 	}
 
-	T GetValue() const { return m_val; }
+
+	/**
+	 * get the numerical value
+	 */
+	T GetValue() const
+	{ 
+		return m_val;
+	}
+
 
 private:
+	// value
 	T m_val{};
+
+	// precision
 	std::streamsize m_prec{6};
 };
 
