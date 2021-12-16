43a33fa67daf55715bf5d05ee951c7abb170fed0
Tobias WEBER <tweber@ill.fr>
2021-12-16 15:45:28 +0100

    more detector shape tests

--- tests/detshape.cpp
+++ tests/detshape.cpp
@@ -71,6 +71,23 @@ t_vec get_cylindrical_pos(t_real z, t_real phi, t_real rad)
 }
 
 
+t_vec get_flat_pos(t_real z, t_real phi, t_real rad)
+{
+	phi = -phi;
+
+	t_real x = rad*std::tan(phi);
+	t_real y = rad;
+	z = z;
+
+	t_vec vec = tl2::create<t_vec>({x, y, z});
+	t_real len = tl2::norm<t_vec>(vec);
+	//std::cout << len << std::endl;
+	vec /= len;
+
+	return vec;
+}
+
+
 int main()
 {
 	using namespace tl2_ops;
@@ -79,26 +96,40 @@ int main()
 	t_real k = 1.;
 	t_vec ki = tl2::create<t_vec>({0, 1, 0}) * k;
 
+	// select a pixel on the detector
+	t_real det_dist = 5.;
+	t_real z = 2.5;
+	t_real phi = t_real(45) / t_real(180) * tl2::pi<t_real>;
+
+	// get the Q vector corresponding to this pixel
 	{
-		t_real theta = t_real(0) / t_real(180) * tl2::pi<t_real>;
-		t_real phi = t_real(45) / t_real(180) * tl2::pi<t_real>;
+		//t_real theta = t_real(10) / t_real(180) * tl2::pi<t_real>;
+		t_real theta = std::atan2(z, det_dist);
 		t_vec kf = get_spherical_pos(theta, phi) * k;
 
 		t_vec Q = ki - kf;
+		std::cout << "spherical detector\n";
 		std::cout << "ki = " << ki << "\n";
 		std::cout << "kf = " << kf << "\n";
 		std::cout << "Q = " << Q << std::endl;
 	}
 
-
 	{
-		t_real z = 0.;
-		t_real rad = 5.;
-		t_real phi = t_real(45) / t_real(180) * tl2::pi<t_real>;
-		t_vec kf = get_cylindrical_pos(z, phi, rad) * k;
+		t_vec kf = get_cylindrical_pos(z, phi, det_dist) * k;
 
 		t_vec Q = ki - kf;
-		std::cout << "\nki = " << ki << "\n";
+		std::cout << "\ncylindrical detector\n";
+		std::cout << "ki = " << ki << "\n";
+		std::cout << "kf = " << kf << "\n";
+		std::cout << "Q = " << Q << std::endl;
+	}
+
+	{
+		t_vec kf = get_flat_pos(z, phi, det_dist) * k;
+
+		t_vec Q = ki - kf;
+		std::cout << "\nflat detector\n";
+		std::cout << "ki = " << ki << "\n";
 		std::cout << "kf = " << kf << "\n";
 		std::cout << "Q = " << Q << std::endl;
 	}
