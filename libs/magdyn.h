cdd0fba468a6e7309d39396474d06ab977ea0b06
Tobias WEBER <tweber@ill.fr>
2022-03-03 13:24:07 +0100

    cosmetic change

--- libs/magdyn.h
+++ libs/magdyn.h
@@ -8,6 +8,7 @@
  * References:
  *   - (Toth 2015) S. Toth and B. Lake, J. Phys.: Condens. Matter 27 166002 (2015):
  *                 https://doi.org/10.1088/0953-8984/27/16/166002
+ *                 https://arxiv.org/abs/1402.6069
  *   - (Heinsdorf 2021) N. Heinsdorf, manual example calculation for a simple
  *                      ferromagnetic case, personal communications, 2021/2022.
  *
